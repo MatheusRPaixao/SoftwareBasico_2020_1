80a6620213812523d8f3f67fe2b19b853763ff5d
Matheus Paixão <matheuspaixao97@gmail.com>
2020-04-14 20:05:51 -0300

    5

--- src/conv_utf.c
+++ src/conv_utf.c
@@ -9,11 +9,13 @@
  * 
  * Version control:
  * Id       Day         Version         Description
- * 1        08-04       0.0.1           Question comprehension and did the base coding to develop the functions utf8_32 and utf32_8 
+ * 1        08-04       0.0.1           Question comprehension and base code to develop the functions utf8_32 and utf32_8 
  * 2        10-04       0.0.2           Minors updates and Headers creation
- * 3        11-04       0.1.3           Minors updates and function utf8_32 implemented
- * 4        13-04       0.1.4           Minors updates and optmizations, readable code 
+ * 3        11-04       0.1.0           Minors updates and function utf8_32 implemented
+ * 4        13-04       0.1.1           Minors updates and optmizations, readable code 
+ * 5        14-04       0.2.0           Enum to Errors, add regions,
  */
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,44 +23,61 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
-#include "conv_utf.h"
+#include "../headers/conv_utf.h" 
 
 #define MAXPATHSIZE 1024
+#define TRUE 1
+#define FALSE 0
+#define BIGBOOM 0x0000feff
+#define LITTLEBOOM 0xfffe0000 
 
-/* Private metods */
 
+/* Private methods */
+//Criar enum para erros
 /**
- *  Function to verify if is a 8UTF file
+ *  Method to verify if given file is a 8UTF coded file
  *  
  *  Parameters:
  *      inFile - the input file to be checked
  * 
  *  Return:
- *  boolean-ish result 
+ *  boolean result 
  *  if the file is a 8UTF type file return 1
  *  else 0  
  *  
  * */
-int Is8utf(FILE* inFile); /* main */
+int Is8Utf(FILE* inAuxFile); /* main */
 
 /* add Description */
 int IdentifyCharSize(unsigned char inbyte);
 
 /* add Description */
-unsigned long GetUnicodeFromUtf8(unsigned char inByte, unsigned char auxInByte[3], int n);
+unsigned int GetUnicodeFromUtf8(unsigned char inByte, unsigned char auxInByte[3], int n);
+
+/* add Description */
+unsigned int BigToLittle(unsigned int outByte);
 
 
 /* ************** */
 
+#pragma region Public Methods 
 
+int utf32_8(FILE* inFile, FILE* outFile)
+{
+    return 0 ;
+}
 
 int utf8_32(FILE* inFile, FILE* outFile)
 {
     unsigned char inByte;
     unsigned char auxInByte[3];
-    unsigned long outByte;
+    unsigned int outByte;
 
     int numBytesToRead;
+    unsigned int little = 0xfffe0000;
+
+    /* Introduces BOOM Byte in Utf32 File */
+    fwrite( little,sizeof(outByte) ,1 ,outFile);
 
     while(fread(inByte, sizeof(inByte), 1, inFile))
     {
@@ -66,30 +85,34 @@ int utf8_32(FILE* inFile, FILE* outFile)
         { 
             if (!fread(auxInByte, numBytesToRead, 1, inFile))
             {
-                puts('Fail to identify size of char ');
+                perror("Fail to identify size of char ");
                 exit(0);
             }
         }
 
-        outByte = GetUnicodeFromUtf8(inByte, auxInByte, numBytesToRead);
-        /* tranformar para unsigned long  */
+        outByte = BigToLittle(GetUnicodeFromUtf8(inByte, auxInByte, numBytesToRead));
+        /* tranformar para unsigned int  */
         /* retornar 1  */
 
         if (!fwrite(outByte, sizeof(outByte), 1, outFile))
         {
-            puts('Error writting at new file ');
+            perror("Error writting at new file ");
             exit(0);
         }
     }
 
 }
 
+#pragma endregion
+
+
+#pragma region Private Methods
 
 int IdentifyCharSize(unsigned char inByte)
 {
     unsigned char aux ;
-    int ret = 0;
-    int loop = 1;
+    int ret = FALSE ;
+    int loop = TRUE ;
 
     while (loop)
     {
@@ -101,7 +124,13 @@ int IdentifyCharSize(unsigned char inByte)
         }
         else
         {
-            loop = 0;
+            loop = FALSE;
+        }
+
+        if (loop > 4)
+        {
+            perror("Erro to identify given byte");
+            exit(0);
         }
         
     }
@@ -109,30 +138,67 @@ int IdentifyCharSize(unsigned char inByte)
     return ret ;
 }
 
-unsigned long GetUnicodeFromUtf8(unsigned char inByte, unsigned char auxInByte[3], int n)
+unsigned int GetUnicodeFromUtf8(unsigned char inByte, unsigned char auxInByte[3], int n)
 {
-    unsigned long ret;
+    unsigned int ret;
     unsigned char aux;
     int desloc = 6;
     int cont;
 
     ret += ((inByte<<(n+1))>>(8 - (n+1)));
-    ret = ret<<desloc;
 
-    for (cont = 0; cont < n; cont++)
+    /* if n is positive then is a multiple byte char */
+    if (n)
     {
-        aux = (auxInByte[cont]<<2)>>2;
+        ret = ret<<desloc;
 
-        ret+=aux;
-        ret=ret<<desloc;
+        for (cont = 0; cont < n; cont++)
+        {
+            aux = (auxInByte[cont]<<2)>>2;
+
+            ret+=aux;
+            ret=ret<<desloc;
+        }
     }
+
     return ret;
-
-
 
 }
 
+unsigned int BigToLittle(unsigned int outByte)
+{
+    unsigned int ret = 0x00;
+    unsigned int aux ;
+    int cont, desloc = 4 ;
 
+    for (cont = 0; cont < sizeof(outByte)*8; cont += desloc)
+    {
+        aux = outByte;
+        aux = (aux >> cont) & 0x0f;
+
+        ret += aux ;
+        ret = ret << desloc;
+    }
+
+    return ret;
+
+}
+
+int Is8Utf(FILE* inAuxFile)
+{
+    unsigned int firstChar[1];
+
+    fread(firstChar, sizeof(firstChar), 1, inAuxFile);
+
+    if (firstChar[0] != LITTLEBOOM && firstChar[0] != BIGBOOM)
+    {
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+#pragma endregion
 
 
 int main ()
@@ -142,20 +208,20 @@ int main ()
     struct dirent* inFileName;
     char cwd[MAXPATHSIZE];
     char inFilesDir[MAXPATHSIZE], outFilesDir[MAXPATHSIZE] ;
-    char inFilePath[MAXPATHSIZE], outFilePath[MAXPATHSIZE]; 
-    FILE* inFile, *outFile;
+    char inFilePath[MAXPATHSIZE], outFilePath[MAXPATHSIZE] ; 
+    FILE* inFile, *outFile, *inAuxFile;
 
     /* get current directory */
     getcwd(cwd, sizeof(cwd));
 
     /* defines origin of files to be converted and destination for the converted files */
     sprintf(inFilesDir, "%s/%s", cwd, "toConvert");
-    sprintf(outFilesDir, "%s/%s", cwd,"converted");
+    sprintf(outFilesDir, "%s/%s", cwd, "converted");
 
     /*  list of all arq's to be converted */
     if (!(filesToConvert = opendir(inFilesDir)))
     {
-        printf("Fail to open directory %s , please read the README file\n", inFilePath);
+        perror("Error open directory \"/toConvert\", please read the README.md file");
         exit(0);
     }
 
@@ -168,22 +234,26 @@ int main ()
             sprintf(inFilePath, "%s/%s" ,inFilesDir,inFileName->d_name);
             sprintf(outFilePath, "%s/%s", outFilesDir,inFileName->d_name);
 
+            /* Needed to verify BOOM char */
+            inAuxFile = fopen(inFilePath, "rb");
+
             /* Open respective in and out files */
             inFile = fopen(inFilePath, "rb");
             outFile = fopen(outFilePath, "wb");
 
-            if (Is8utf(inFile))
+            if (Is8Utf(inAuxFile))
             {
                 utf8_32(inFile, outFile);
             }
             else 
             {
-                utf32_8(inFile,outFile);
+                utf32_8(inFile,outFile); /* TODO */
             }
 
             /* Close respective in and out files */
             fclose(inFile);
             fclose(outFile);
+            fclose(inAuxFile);
 
         }
     }
